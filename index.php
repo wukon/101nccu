51ce86813990d4157e3832170fb667de66366f90
wukon <wukonkimo@gmail.com>
2016-01-06 21:52:24 +0800

    x
    x

--- index.php
+++ index.php
@@ -67,8 +67,8 @@ imagecopyresized($thumb, $src, 0, 0, 0, 0, $thumb_w, $thumb_h, $src_w,    $src_h
 // save thumbnail
 imagejpeg($thumb, 'abc.jpg');
 
-$input =$s3->inputResource(fopen('abc.jpg',"rb"), filesize('abc.jpg');
-if(S3::putObject($input,'smalls3', $filename , S3::ACL_PUBLIC_READ))
+$input =$s3->inputResource(fopen('abc.jpg',"rb"), filesize('abc.jpg'));
+if(S3::putObject($input,'smalls3', $filename , S3::ACL_PUBLIC_READ ))
 
 {
 	echo "file upload success";
