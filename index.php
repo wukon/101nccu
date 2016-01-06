58b4456d14531157ed4a28d445d621b6ee339d66
wukon <wukonkimo@gmail.com>
2016-01-06 20:45:11 +0800

    dfd
    dd

--- index.php
+++ index.php
@@ -37,6 +37,10 @@
 			'password' => parse_url($_ENV['REDIS_URL'], PHP_URL_PASS),
 		));
 
+		
+		
+
+
 		/*  cache client 引入及設定  */
 		// Using MemcacheSASL client
 		/*
@@ -59,6 +63,20 @@
 		if (!defined('awsSecretKey')) define('awsSecretKey', getenv('S3_SECRET'));
 		//instantiate the class
 		$s3 = new S3(awsAccessKey, awsSecretKey);
+		
+		// 引入 SQS class 並 initiate
+// $sqs = new SQS('Access Key Here', 'Secret Key Here');
+require_once('sqs.php');
+$sqs = new SQS(awsAccessKey, awsSecretKey);
+
+// 先設定好 queue 的位置
+$input_queue = "https://sqs.us-west-2.amazonaws.com/500101126759/input";
+
+
+
+/******
+	上傳檔案到 S3（等待 process 程式處理）
+******/
 		/* 外部 S3 class 設定 */
 
 		/* 上傳表單 post 回同一頁，判斷如果有上傳把檔案接回來 */
@@ -84,6 +102,14 @@
 
 					$redis->set($fileName, $data);
 
+					
+					// 範例：把目前時間當成 message 送給 input queue
+                    // （縮圖程式的 message 內容改為圖片位置以及希望得到的 size 等資訊）
+                    date_default_timezone_set('Asia/Taipei');
+                    $send = $sqs->sendMessage($input_queue, $fileName );
+                    echo "Message Send: <br>";
+                    print_r($send);
+                     echo "<br>";
 		    		// saving file on S3
 					if ($s3->putObjectFile($fileTempName, "nccus3", $fileName, S3::ACL_PUBLIC_READ)) {
 					    echo "We successfully uploaded your file.";  
